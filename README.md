34dafe958aa26fb856001fa60ae278dc1009338d
dhhill <dhhillaz@gmail.com>
2017-10-19 20:08:52 -0700

    Editing readme and adding hash function

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Blockchain
+
+
+
+> coffee is delicious. - Daniel Hill.
+
+#
+
+| Header One     | Header Two     |
+| :------------- | :------------- |
+| Item One       | Item Two       |
+
+- [ ] get this done
+
+## yo
+### hi
--- hashfunc.py
+++ hashfunc.py
@@ -4,9 +4,8 @@ def hashMe(msg=""):
     # For convenience, this is a helper function that wraps our hashing algorithm
     if type(msg) != str:
         msg = json.dumps(msg,sort_keys=True)    # if we don't sort keys, we can't guarantee repeatability!
-    
+
     if sys.version_info.major == 2:
         return unicode(hashlib.sha256(msg).hexdigest(), 'utf-8')
     else:
         return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
-
